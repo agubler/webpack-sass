3464e4969c88cc4e0e9b577bc3274e7ace590c44
Anthony Gubler <agubler@sitepen.com>
2017-06-28 02:31:48 +0100

    build with sass

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+
+const div = document.createElement("div");
+const body = document.getElementsByTagName('body')[0];
+div.className = 'myDiv';
+body.appendChild(div);
+
---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+import webpack from 'webpack';
+import * as path from 'path';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+	entry: [ './src/app.ts', './src/app.scss'],
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				loader: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: "css-loader!sass-loader",
+				}),
+			},
+			{
+				test: /\.ts$/,
+				use: 'ts-loader'
+			},
+		],
+	},
+	plugins: [
+		new ExtractTextPlugin("app.css"),
+		new HtmlWebpackPlugin({
+			template: 'src/index.html',
+			inject: true,
+		})
+	],
+	resolve: {
+		extensions: [ '.ts', '.js' ],
+		modules: [
+			path.resolve('./src'),
+			'node_modules'
+		]
+	},
+	output: {
+		path: path.resolve('./dist'),
+		filename: '[name].js'
+	},
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+		compress: true,
+		port: 9000
+	}
+};
+
+export default config;
